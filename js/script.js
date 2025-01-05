d069d2afd03ff2370ee31fe34d77e2b540d0596d
Serfer-m4 <serfer.tt.tm@gmail.com>
2025-01-05 13:43:15 +0200

    FIX

---
+++ js/script.js
@@ -0,0 +1,58 @@
+
+let age = 30
+
+const digit = 5
+console.log(typeof digit)
+
+const stroke = 'Happy New Year'
+console.log(typeof stroke)
+
+const bool = true
+console.log(typeof bool)
+
+const nulish = null
+console.log(typeof nulish)
+
+const undef = undefined //
+console.log(typeof undef)
+
+const bigInteger = 7538475439576947684957945125
+console.log(typeof bigInteger)
+
+const symb = Symbol('unieqeId')
+console.log(typeof symb)
+
+const obj = {
+    name: "Serfer",
+    age: 36,
+    array:[]
+};
+
+console.log(obj.name)
+console.log(obj);
+console.log(typeof obj)
+
+const fruits = ['apple', 'orange', 'banana']
+
+
+const obj2 = obj
+
+obj2.name = 'Serhii'
+
+console.log('obj', obj)
+console.log('obj2', obj2)
+
+fruits.push('grape')
+console.log(fruits)
+
+//  унарні та бінарні оператори
+
+ const sum = 3 + 5 
+
+ // increament
+ // decreament
+let x = 5;
+let y = 10;
+
+console.log(x < y)
+console.log (y < x)
