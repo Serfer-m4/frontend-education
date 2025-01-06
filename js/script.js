65a45e346f36a2dc10b5a9935b1a10c5f11a286d
Serfer-m4 <serfer.tt.tm@gmail.com>
2025-01-07 00:29:42 +0200

    Homework 3

--- js/script.js
+++ js/script.js
@@ -1,58 +1,64 @@
-
-let age = 30
-
-const digit = 5
-console.log(typeof digit)
+// 3.1 Перелік типів
+console.log("3.1 Перелік типів:")
+const digit = 5 
+console.log(typeof digit); // Number
 
 const stroke = 'Happy New Year'
-console.log(typeof stroke)
+console.log(typeof stroke); //String
 
 const bool = true
-console.log(typeof bool)
+console.log(typeof bool); // boolean
 
 const nulish = null
-console.log(typeof nulish)
+console.log(typeof nulish); // object
 
-const undef = undefined //
-console.log(typeof undef)
+const undef = undefined 
+console.log(typeof undef); //undefind
 
-const bigInteger = 7538475439576947684957945125
-console.log(typeof bigInteger)
+const bigInteger = 7538475439576947684957945125n
+console.log(typeof bigInteger); // bigint 
 
 const symb = Symbol('unieqeId')
-console.log(typeof symb)
+console.log(typeof symb); //symbol
+
 
 const obj = {
     name: "Serfer",
     age: 36,
-    array:[]
+    job: "it"
 };
 
-console.log(obj.name)
-console.log(obj);
-console.log(typeof obj)
+console.log(typeof obj); // object
+console.log(obj); 
+console.log(obj.name, obj.age, obj.job );
 
-const fruits = ['apple', 'orange', 'banana']
+// const obj2 = obj
 
+// obj2.name = 'Serhii'
 
-const obj2 = obj
+// console.log('obj', obj);
+// console.log('obj2', obj2);
 
-obj2.name = 'Serhii'
+// const fruits = ['apple', 'orange', 'banana'];
+// fruits.push('grape');
 
-console.log('obj', obj)
-console.log('obj2', obj2)
+// console.log(fruits);
 
-fruits.push('grape')
-console.log(fruits)
+// 3.2. Числа та рядки
+console.log("3.2. Числа та рядки:")
+const line1 = 'Lorem ipsum dolor sit amet'
+const line2 = 'consectetur adipiscing elit'
+const line3 = 'sed do eiusmod tempor incididunt ut labore et'
+console.log(line2 + line3 + line1); 
 
-//  унарні та бінарні оператори
-
- const sum = 3 + 5 
-
- // increament
- // decreament
-let x = 5;
-let y = 10;
-
-console.log(x < y)
-console.log (y < x)
+// 3.3. Розкласти п'ятизначне число
+console.log("3.3. Розкласти п'ятизначне число:")
+const index = 77531
+function splitNumber(index) {
+    return index
+        .toString()
+        .split("")
+        .join(" ")
+}
+const splindex = splitNumber(index);
+console.log(splindex); // 7 7 5 3 1
